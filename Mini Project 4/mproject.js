2e2bfe7da230d4fc4008d3ac863221247f386888
n <n@Wilhuff-Tarkin.fios-router.home>
2026-03-03 11:41:13 -0500

    Mini Project 4, stringCompare

---
+++ Mini Project 4/mproject.js
@@ -0,0 +1,54 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+/*
+fish - {
+    name: string
+    weight: number
+    value: number
+}
+
+*/
+
+const generateRandomName = () => {
+    const adj1Arr = ["Large", "Small", "Scaly", "Slimy", "Quick"];
+    const adj2Arr = ["Smooth", "Wild", "Slippery", "Gentle", "Striped"];
+    const typeArr = ["Salmon", "Trout", "Catfish", "Bass", "Cod"];
+
+    // adj1 adj2 type
+    // get random adj1 - get a random item from an array
+    // 0 - 1 * 5   0 - 5 -> Math.floor -> 0 - 4
+    let adj1Index = Math.floor(Math.random() * adj1Arr.length);
+    let adj2Index = Math.floor(Math.random() * adj2Arr.length);
+    let typeIndex = Math.floor(Math.random() * typeArr.length);
+
+    let adj1 = adj1Arr[adj1Index];
+    let adj2 = adj2Arr[adj2Index];
+    let type = typeArr[typeIndex];
+
+    return `${adj1} ${adj2} ${type}`;
+}
+
+const generateRandomWeight = () => {
+    // .toFixed(num) - returns a (string)decimal based on how many specified places (num) that you pass in
+    // .toFixed(2) -> 2 decimal places
+    // .toFixed(3) -> 3 decimal places etc.
+    return Number(Math.random() * 5).toFixed(2);
+}
+
+const generateRandomFish = () => {
+    // random name
+    let fishName = generateRandomName();
+    // random weight
+    let fishWeight = generateRandomWeight();
+    // random value
+
+    // our new fish object
+    let newFish = {
+        name: fishName,
+        weight: fishWeight
+    }
+
+    console.log(newFish)
+}
+
+generateRandomFish();
---
+++ Practice Problem/stringCompareCounter.js
@@ -0,0 +1,35 @@
+/*
+stringCompareCounter
+Write a function called stringCompareCounter that takes in 2 strings of the same length as parameters. return a count of how many characters the strings share at the same index.
+
+Examples:
+
+//(strings share the same character at index 0)
+stringCompareCounter('cat','cow') => 1 
+
+//(strings share the same characters at indexes 1 and 2)
+stringCompareCounter('count','touch') => 2 
+
+//(strings share same characters at indexes 1,2, and 3)
+stringCompareCounter('well','sell') => 3 
+
+*/
+
+const stringCompareCounter = (str1, str2) => {
+    let count = 0;
+
+    for(let i = 0; i < str1.length; i++){
+        if(str1[i] === str2[i]){
+            // they have the same character at the same index
+            count++;
+        }
+    }
+
+    return count;
+
+}
+
+console.log(stringCompareCounter('cat','cow'))
+console.log(stringCompareCounter('count','touch'))
+console.log(stringCompareCounter('wall','sell'))
+console.log(stringCompareCounter('walk','seal'))
