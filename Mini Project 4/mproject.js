2c401cda0eb4a88c69af0c13ed48d47c5bcd8a43
Yahaira R <yhrodriguez20@gmail.com>
2026-03-03 16:30:17 -0500

    Mini Porject 4 update

--- Mini Project 4/mproject.js
+++ Mini Project 4/mproject.js
@@ -6,59 +6,104 @@ fish - {
     weight: number
     value: number
 }
-
 */
 
 const generateRandomName = () => {
-    const adj1Arr = ["Large", "Small", "Scaly", "Slimy", "Quick"];
-    const adj2Arr = ["Smooth", "Wild", "Slippery", "Gentle", "Striped"];
-    const typeArr = ["Salmon", "Trout", "Catfish", "Bass", "Cod"];
+  const adj1Arr = ["Large", "Small", "Scaly", "Slimy", "Quick"];
+  const adj2Arr = ["Smooth", "Wild", "Slippery", "Gentle", "Striped"];
+  const typeArr = ["Salmon", "Trout", "Catfish", "Bass", "Cod"];
 
-    // adj1 adj2 type
-    // get random adj1 - get a random item from an array
-    // 0 - 1 * 5   0 - 5 -> Math.floor -> 0 - 4
-    let adj1Index = Math.floor(Math.random() * adj1Arr.length);
-    let adj2Index = Math.floor(Math.random() * adj2Arr.length);
-    let typeIndex = Math.floor(Math.random() * typeArr.length);
+  // adj1 adj2 type
+  // get random adj1 - get a random item from an array
+  // 0 - 1 * 5   0 - 5 -> Math.floor -> 0 - 4
+  let adj1Index = Math.floor(Math.random() * adj1Arr.length);
+  let adj2Index = Math.floor(Math.random() * adj2Arr.length);
+  let typeIndex = Math.floor(Math.random() * typeArr.length);
 
-    let adj1 = adj1Arr[adj1Index];
-    let adj2 = adj2Arr[adj2Index];
-    let type = typeArr[typeIndex];
+  let adj1 = adj1Arr[adj1Index];
+  let adj2 = adj2Arr[adj2Index];
+  let type = typeArr[typeIndex];
 
-    return `${adj1} ${adj2} ${type}`;
-}
+  return `${adj1} ${adj2} ${type}`;
+};
 
 const generateRandomWeight = () => {
-    // .toFixed(num) - returns a (string) decimal based on how many specified places (num) that you pass in
-    // .toFixed(2) -> 2 decimal places
-    // .toFixed(3) -> 3 decimal places etc.
-    return Number((Math.random() * 5).toFixed(2));
-}
+  // .toFixed(num) - returns a (string) decimal based on how many specified places (num) that you pass in
+  // .toFixed(2) -> 2 decimal places
+  // .toFixed(3) -> 3 decimal places etc.
+  return Number((Math.random() * 5).toFixed(2));
+};
 
 const generateRandomValue = () => {
-    return Number((Math.random() * 15).toFixed(2));
-}
+  return Number((Math.random() * 15).toFixed(2));
+};
 
 const generateRandomFish = () => {
-    // random name
-    let fishName = generateRandomName();
-    // random weight
-    let fishWeight = generateRandomWeight();
-    // random value
-    let fishValue = generateRandomValue();
+  // random name
+  let fishName = generateRandomName();
+  // random weight
+  let fishWeight = generateRandomWeight();
+  // random value
+  let fishValue = generateRandomValue();
 
-    // our new fish object
-    let newFish = {
-        name: fishName,
-        weight: fishWeight,
-        value: fishValue
-    }
+  // our new fish object
+  let newFish = {
+    name: fishName,
+    weight: fishWeight,
+    value: fishValue,
+  };
 
-    console.log(newFish)
-}
+  //   console.log(newFish);
+  return newFish;
+};
 
 generateRandomFish();
 
 // creating a UI with console.logs/prompts like we did for the to-do list
 // make it so player can catch OR release fish
 // 6 turns long
+
+console.log(`You've gone fishing! Try to maximize the value of your caught fish. You can fish
+for six hours (till 12:00pm) and can catch at most 10 lbs of fish.`);
+
+console.log("\n==========================================\n");
+
+let hour = 6;
+
+// Array to keep track of caught fish
+let caughtArray = [];
+let totalWeight = 0;
+let totalValue = 0;
+
+while (hour < 12) {
+
+  // total weight and total value
+  console.log(`The time is ${hour}:00am. So far you've caught:
+${caughtArray.length} fish, ${totalWeight} lbs, $${totalValue}`);
+  let currentFish = generateRandomFish();
+
+  console.log(`You caught a '${currentFish.name}' weighing ${currentFish.weight} lbs
+and valued at $${currentFish.value}\n`);
+
+  console.log(`Your action: [c]atch or [r]elease?`);
+  let choice = prompt("> ");
+
+  if (choice === "c") {
+    console.log("\nYou chose to catch the fish");
+    caughtArray.push(currentFish);
+
+    totalWeight = Number((totalWeight + currentFish.weight).toFixed(2));
+    totalValue = Number((totalValue + currentFish.value).toFixed(2));
+  } else {
+    console.log("\nYou chose to release the finish");
+  }
+
+  console.log("\n==========================================\n");
+  hour++;
+}
+
+// keep track of fish caught
+// keep track of totals that need to be displayed to player every turn
+
+// totals for when the game is over
+// prevent the user from catching over 10 lbs
