dd46e40d21d4fd68a94c7188cbbe2100fdf26bbc
Yahaira R <yhrodriguez20@gmail.com>
2026-03-04 16:47:12 -0500

    Practice Problem- titleCase.js

--- Practice Problem/titleCase.js
+++ Practice Problem/titleCase.js
@@ -1,6 +1,6 @@
 /*
 
-/* titleCase - returns a transformed version of the given string where every word starts with a capital letter and every non-word-starting letter is lowercased
+titleCase - returns a transformed version of the given string where every word starts with a capital letter and every non-word-starting letter is lowercased
 
 titleCase('return of the king') -> 'Return Of The King'
 titleCase('NOblE desKtoP') -> "Noble Desktop"
